bdf39ef61ed7eed43b49e872e2edf16a1e11482e
Taylored9452 <TaylorWalker9452@gmail.com>
2024-03-26 01:05:04 +0700

    hot fix

--- add.php
+++ add.php
@@ -2,6 +2,8 @@
 
 <?php 
 
+    $sql_provinces = "SELECT * FROM provinces";
+    $query = mysqli_query($conn, $sql_provinces);
 // ตรวจสอบว่าฟอร์มถูกส่งมาหรือไม่
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     // เก็บข้อมูลจากฟอร์ม
@@ -21,7 +23,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     FROM tbuser as u
     join tbhistoryuser as htu on u.historyuserid = htu.historyuserid
     join tbprefix as p on htu.prefixid = p.prefixid";
-
     $result1 = mysqli_query($conn, $sql1) or die ("Error in query: $sql1" . mysqli_error());
     
     $sql2 = "INSERT INTO tbfirstname (firstnameid, firstnamename) VALUES ('', '$firstnamename')";
@@ -30,12 +31,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql3 = "INSERT INTO tblastname (lastnameid, lastnamename) VALUES ('', '$lastnamename')";
     $result3 = mysqli_query($conn, $sql3) or die ("Error in query: $sql3" . mysqli_error());
 
-
+    
+    
     mysqli_close($conn);
 
     // ตรวจสอบว่าการแทรกสำเร็จหรือไม่
     // if ($result1 && $result2 && $result3 ){
-    if ($result1){
+    if ($result4){
         echo "<script type='text/javascript'>";
         echo "alert('successfully');";
         echo"window.location = 'add.php';";
@@ -60,11 +62,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Bootstrap Sidebar 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style-add.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <!-- เนื้อหา HTML รวมถึงฟอร์มสำหรับเพิ่มรายการผู้ใช้ใหม่ -->
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <select name="tbprefix" class="custom-dropdown">
+        <select name="tbprefix" class="custom-dropdown">
             <?php
             // วนลูปเพื่อแสดงตัวเลือกใน dropdown
             $prefixaname = mysqli_query($conn, "Select * from tbprefix");
@@ -73,9 +77,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             <option value="<?php echo $row['prefixid'] ?>"><?php echo $row['prefixname'] ?></option>
             <?php } ?>
         </select><br><br>
+
         <input type="text" name="firstnamename" placeholder="ชื่อ"><br>
         <input type="text" name="lastnamename" placeholder="นามสกุล"><br>
         <input type="text" name="useraddress" placeholder="ที่อยู่"><br>
+
+        <label for="sel1">จังหวัด:</label>
+        <select class="form-control" name="Ref_prov_id" id="provinces">
+            <option value="" selected disabled>-กรุณาเลือกจังหวัด-</option>
+             <?php foreach ($query as $value) { ?>
+            <option value="<?=$value['id']?>"><?=$value['name_th']?></option>
+            <?php } ?>
+      </select>
+      <br>
+ 
+      <label for="sel1">อำเภอ:</label>
+      <select class="form-control" name="Ref_dist_id" id="amphures">
+      </select>
+      <br>
+ 
+      <label for="sel1">ตำบล:</label>
+      <select class="form-control" name="Ref_subdist_id" id="districts">
+      </select>
+       <br>
+ 
+      <label for="sel1">รหัสไปรษณีย์:</label>
+       <input type="text" name="zip_code" id="zip_code" class="form-control">
+          <br>
+
         <input type="email" name="emailusername" placeholder="อีเมล"><br>
         <input type="text" name="phoneusername" placeholder="เบอร์ติดต่อ"><br>
         <input type="text" name="campusname" placeholder="วิทยาเขต"><br>
@@ -93,3 +122,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     </form>
 </body>
 </html>
+<?php include('script.php');?>
